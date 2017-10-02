c4b656a8d695f5514a479dd84eb901ad8471fce9
rgh <sundrops.ren@gmail.com>
2017-10-03 01:03:41 +0800

    opencv3.0.0(cuda8.0)编译安装

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # opencv_3.0.0
+## 说明
+cuda8.0+opencv_3.0.0
+修改了部分原版的opencv3.0以便和cuda8.0兼容
+- modules/cudalegacy/src/graphcuts.cpp
+
+```c++
+// 注释下面这句
+#if !defined (HAVE_CUDA) || defined (CUDA_DISABLER)
+// 改为下面这句
+// GraphCut has been removed in NPP 8.0
+#if !defined (HAVE_CUDA) || defined (CUDA_DISABLER) || (CUDART_VERSION >= 8000)
+
+```
+- /home/rgh/software/opencv-3.0.0/3rdparty/ippicv/downloads/linux-xxx中自带了ippicv_linux_20141027.tgz，自己下载太慢，xxx可能有变，拷贝到对应目录就好
+
+## 编译
+
+- 安装依赖
+
+```bash
+sudo sh dependencies.sh
+```
+
+- 编译
+
+```bash
+mkdir build
+cd build
+# cmake此处就会卡主下载上面说的ippicv_linux_20141027.tgz，把这个文件拷贝到你对应的opencv-3.0.0/3rdparty/ippicv/downloads/linux-xxx中
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make -j 16
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+```
+- 查看opencv版本
+
+```bash
+pkg-config --modversion opencv
+# 显示3.0.0，为OK
+```
---
+++ dependencies.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "--- Removing any pre-installed ffmpeg and x264"
+sudo apt-get -qq remove ffmpeg x264 libx264-dev
+apt-get -y install libopencv-dev
+apt-get -y install build-essential
+apt-get -y install checkinstall
+apt-get -y install cmake
+apt-get -y install pkg-config
+apt-get -y install yasm
+apt-get -y install libtiff5-dev
+apt-get -y install libjpeg-dev
+apt-get -y install libjasper-dev
+apt-get -y install libavcodec-dev
+apt-get -y install libavformat-dev
+apt-get -y install libswscale-dev
+apt-get -y install libdc1394-22-dev
+apt-get -y install libxine2-dev
+apt-get -y install libgstreamer0.10-dev
+apt-get -y install libgstreamer-plugins-base0.10-dev
+apt-get -y install libv4l-dev
+apt-get -y install python-dev
+apt-get -y install python-numpy
+apt-get -y install libtbb-dev
+apt-get -y install libqt5x11extras5
+apt-get -y install libqt5opengl5
+apt-get -y install libqt5opengl5-dev
+apt-get -y install libgtk2.0-dev
+apt-get -y install libfaac-dev
+apt-get -y install libmp3lame-dev
+apt-get -y install libopencore-amrnb-dev
+apt-get -y install libopencore-amrwb-dev
+apt-get -y install libtheora-dev
+apt-get -y install libvorbis-dev
+apt-get -y install libxvidcore-dev
+apt-get -y install x264
+apt-get -y install v4l-utils
+#apt-get -y install ffmpeg
+apt-get -y install unzip
